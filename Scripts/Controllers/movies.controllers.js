7f6454150a220fdccb0fe34ba1c11db6b315ac60
Ty Thorton <tyt@usa-it-28lfh01.corp.unicity.net>
2017-01-27 07:24:49 -0700

    Fixed Api so we can change to specific movie

--- Scripts/Controllers/movies.controllers.js
+++ Scripts/Controllers/movies.controllers.js
@@ -3,32 +3,35 @@
         angular.module('myApp')
             .component('movie', { // the tag for using this is <char-detail>
                 templateUrl: "Templates/movie.template.html",
-                controller: homePageController,
+                controller: moviePageController
                
 
 
             })
-             .config(homePageConfig):
+            //  .config(homePageConfig):
 
-            function moviePageConfig($stateprovider){
-                $stateprovider.state('movie', {
-                    url:'/movie',
-                    template: '<movie></movie>'
-                });
+            // function moviePageConfig($stateprovider){
+            //     $stateprovider.state('movie', {
+            //         url:'/movie',
+            //         template: '<movie></movie>'
+            //     });
 
-        function homePageController(movieService) {
+        function moviePageController(movieService ,$http) {
             var vm = this;
             // vm.movieName = movieName;
             vm.send = send;
+            // vm.method = 'POST';
+            // vm.url = 'Scripts/APIs/movieAPI.js';
             
 
             function send(movieName) {
-                // body..
+
+                            console.log(movieService.getMovie(movieName));
+                // movieService.prepForBroadcast(movieName);
                 // console.log(movieName);
-                vm.movieName = movieName;
-                movieService.addP
+                movieName = movieName;
+                // movieService.add
             }
-            console.log(movieService);
 
             // var promise = movieService.getMovie();
             // promise.then(function (data){
@@ -38,6 +41,4 @@
 
         }
             
-    }
-
-    )();
+    })();
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function(){
 
     // this CREATES the myApp module (because it has the second, array parameter)
-<
+
     angular.module('myApp', [  'ui.router' /* listed imported modules here */])
         .config(myAppConfig);
 
